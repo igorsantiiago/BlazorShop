96416102f71096b1d2c05bab84dd896cb430909e
Igor Santiago <contato.igorsantiago@outlook.com>
2023-07-26 18:13:10 -0300

    Prodcuts and Login pages added

---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorShop.Models;
+
+public class Product
+{
+    public Product() { }
+    public Product(int id, string name, string description, string image, decimal price, int categoryId)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Image = image;
+        Price = price;
+        CategoryId = categoryId;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+
+    public Category Category { get; set; } = new();
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# RazorShop
+
+### 🚧Em desenvolvimento🚧
